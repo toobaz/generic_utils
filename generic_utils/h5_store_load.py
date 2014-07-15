3de3a1b0782e5995edd5407d6d19fdcca5c5f8be
Pietro Battiston <me@pietrobattiston.it>
2014-07-15 09:25:51 +0200

    h5_store_load

---
+++ generic_utils/h5_store_load.py
@@ -0,0 +1,42 @@
+import tables as tb
+from numpy import array
+from scipy import sparse
+
+def store_array(obj, obj_name, store='store.h5'):
+    with tb.openFile(store, 'a') as f:
+        try:
+            n = getattr(f.root, obj_name)
+            n._f_remove()
+        except AttributeError:
+            pass
+        atom = tb.Atom.from_dtype(obj.dtype)
+        ds = f.createCArray(f.root, obj_name, atom, obj.shape)
+        ds[:] = obj
+
+def load_array(obj_name, store='store.h5'):
+    with tb.openFile(store) as f:
+        m = getattr(f.root, obj_name).read()
+        return m
+
+def store_sparse_mat(m, name, store='store.h5'):
+    with tb.openFile(store,'a') as f:
+        for par in ('data', 'indices', 'indptr', 'shape'):
+            full_name = '%s_%s' % (name, par)
+            try:
+                n = getattr(f.root, full_name)
+                n._f_remove()
+            except AttributeError:
+                pass
+            
+            arr = array(getattr(m, par))
+            atom = tb.Atom.from_dtype(arr.dtype)
+            ds = f.createCArray(f.root, full_name, atom, arr.shape)
+            ds[:] = arr
+
+def load_sparse_mat(name, store='store.h5'):
+    with tb.openFile(store) as f:
+        pars = []
+        for par in ('data', 'indices', 'indptr', 'shape'):
+            pars.append(getattr(f.root, '%s_%s' % (name, par)).read())
+    m = sparse.csr_matrix(tuple(pars[:3]), shape=pars[3])
+    return m
