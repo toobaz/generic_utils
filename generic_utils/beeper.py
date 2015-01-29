9e3ca91dac5d42542cb78d52898ca0b4d4886e58
Pietro Battiston <me@pietrobattiston.it>
2015-01-29 19:07:09 +0100

    Beep also on computers without soundcard/gstreamer

--- generic_utils/beeper.py
+++ generic_utils/beeper.py
@@ -8,22 +8,33 @@ class Beeper(object):
     def __init__(self):
         GObject.threads_init()
         Gst.init([])
+        
+        self.sink = Gst.ElementFactory.make('alsasink', None)
+        if not self.sink:
+            # Will try to use the pc speaker instead...
+            return
+        
         self.pipe = Gst.Pipeline()
         self.src = Gst.ElementFactory.make('audiotestsrc', None)
         self.src.set_property('volume', 1)
-        self.pipe.add( self.src )
-
-        self.sink = Gst.ElementFactory.make('alsasink', None)
+        self.pipe.add(self.src)
         self.pipe.add(self.sink)
-
         self.src.link(self.sink)
     
     def beep(self, freq=440):
+        if not self.sink:
+            self.beep_with_beep()
+            return
+        
         self.src.set_property('freq', freq)
         self.pipe.set_state( Gst.State.PLAYING )
         self.ml = GLib.MainLoop()
         GLib.timeout_add( 500, self.stop )
         self.ml.run()
+    
+    def beep_with_beep(self):
+        import subprocess
+        subprocess.call(['beep'])
     
     def stop(self):
         self.pipe.set_state( Gst.State.PAUSED )
