54788dc5e6a6c884529b341121bc1be516728503
Pietro Battiston <me@pietrobattiston.it>
2018-03-12 10:16:14 +0100

    Filter on multiple variables at once

--- generic_utils/pandas/outliers.py
+++ generic_utils/pandas/outliers.py
@@ -2,24 +2,34 @@ import pandas as pd
 import numpy as np
 
 
+def _condition_extreme(based_on, tails):
+    quants = based_on.quantile([tails, 1-tails])
+    return (based_on >= quants.iloc[0]) & (based_on <= quants.iloc[1])
+
 def filter_extreme(data, based_on=None, tails=0.01):
     """
     Filter away extreme observations.
-    "based_on" can be a 1d object of same length than "data", or, if "data" is
-    DataFrame, a key specifiying a column.
+    "based_on" can be
+    - a 1d object of same length than "data"
+    - if "data" is a DataFrame, a key specifiying a column
+    - a list of the above to filter on multiple variables.
 
-    Return binary mask which excludes "tail" % extreme observations on each
-    side on given column in df, on both sides.
+    Return data excluding "tail"% extreme observations on each side on given
+    column in df, on both sides.
     
     """
     if based_on == None:
-        based_on = data
-    elif isinstance(based_on, np.ndarray):
-        based_on = pd.Series(based_on, index=data.index)
-    elif not isinstance(based_on, pd.Series):
-        based_on = data[based_on]
+        based_on = [data]
+    elif not isinstance(based_on, list):
+        based_on = [based_on]
 
-    quants = based_on.quantile([tails, 1-tails])
-    return data.loc[(based_on >= quants.iloc[0]) &
-                    (based_on <= quants.iloc[1])]
+    condition = pd.Series(True, index=data.index)
+    for sub_cond in based_on:
+        if isinstance(based_on, np.ndarray):
+            based_on_s = pd.Series(based_on, index=data.index)
+        elif not isinstance(based_on, pd.Series):
+            based_on_s = data[sub_cond]
+        condition &= _condition_extreme(based_on_s, tails)
+
+    return data.loc[condition]
 
