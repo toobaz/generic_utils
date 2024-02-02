c7c2270201a1fb7369575b848f529ac6fb05ee8e
Pietro Battiston <me@pietrobattiston.it>
2024-02-02 19:02:44 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pb_generic_utils',
+    version='0.0.1',
+    packages='generic_utils,
+    license='MIT',
+    description='A few random convenience functions and classes',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/toobaz/generic_utils',
+    author='Pietro Battiston',
+    author_email='me@pietrobattiston.it'
+)
