68fa31b701e0f3f95a0a4425b8d5b0bd335f56f4
Pietro Battiston <me@pietrobattiston.it>
2024-02-15 11:32:37 +0100

    Typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pb_generic_utils',
     version='0.0.1',
-    packages='generic_utils',
+    packages=['generic_utils'],
     license='MIT',
     description='A few random convenience functions and classes',
     install_requires=['numpy'],
